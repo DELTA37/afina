d93d0bcd9b01894dd1965900ae0a9dc4a3bb931b
delta37 <delta37@delta37.delta37>
2017-09-29 11:23:33 +0300

    move -> add_addr

--- src/allocator/Simple.cpp
+++ src/allocator/Simple.cpp
@@ -16,7 +16,7 @@ static void cleanup(void* base, size_t N) {
   }
 }
 
-static inline void* move(void* base, ptrdiff_t d) {
+static inline void* add_diff(void* base, ptrdiff_t d) {
   return reinterpret_cast<void*>(reinterpret_cast<char*>(base) + d);
 }
 
@@ -45,7 +45,7 @@ std::pair<void*, void*> Simple::findEnoughFree(void* base, size_t N) {
       return std::pair<void*, void*>(NULL, NULL);
     }
     prev = ptr;
-    ptr = move(this->base, cur->diff);
+    ptr = add_diff(this->base, cur->diff);
     cur = info(ptr);
   }
   return std::pair<void*, void*>(prev, ptr);
@@ -58,7 +58,7 @@ void Simple::insertBlock(void* prev, void* base, size_t size) {
   FreeSpace* cur   = info(base);
   FreeSpace* last  = info(prev);
   FreeSpace* new_cur = NULL;
-  FreeSpace* start_info = info(move(this->base, this->size - m));
+  FreeSpace* start_info = info(add_diff(this->base, this->size - m));
   void* new_base = NULL;
 
   if (cur->size < size) {
@@ -68,7 +68,7 @@ void Simple::insertBlock(void* prev, void* base, size_t size) {
     /*
      * Part of the cell is left
      */
-    new_base = move(base, size);
+    new_base = add_diff(base, size);
     if (base == this->last_chain) {
       this->last_chain = new_base;
     }
@@ -122,7 +122,7 @@ void* Simple::findPrevious(void* base, void* mem) {
       return ptr;
     }
     res = ptr;
-    ptr = move(this->base, cur->diff);
+    ptr = add_diff(this->base, cur->diff);
     cur = info(ptr);
   }
   return res;
@@ -133,7 +133,7 @@ bool Simple::isPlaceForPointer(void) {
   FreeSpace* inf = NULL;
   FreeSpace* last_info = info(this->last_chain);
   for (size_t i = this->pointer_info_start; i < this->size - m; i+=m) {
-    inf = info(move(this->base, i));
+    inf = info(add_diff(this->base, i));
     if (inf->diff == 0 && inf->size == 0) {
       return true;
     }
@@ -152,7 +152,7 @@ bool Simple::isPlaceForPointer(size_t size) {
     return false;
   }
   for (size_t i = this->pointer_info_start; i < this->size - m; i+=m) {
-    inf = info(move(this->base, i));
+    inf = info(add_diff(this->base, i));
     if (inf->diff == 0 && inf->size == 0) {
       return true;
     }
@@ -167,10 +167,10 @@ bool Simple::isPlaceForPointer(size_t size) {
 FreeSpace* Simple::registerPointer(ptrdiff_t diff, size_t size) {
   size_t m = sizeof(FreeSpace);
   for (size_t i = this->pointer_info_start; i < this->size - m; i+=m) {
-    FreeSpace* ptr_info = info(move(this->base, i));
+    FreeSpace* ptr_info = info(add_diff(this->base, i));
     if (ptr_info->size == 0) {
-      this->setBlock(move(this->base, i), size, diff);
-      return info(move(this->base, i));
+      this->setBlock(add_diff(this->base, i), size, diff);
+      return info(add_diff(this->base, i));
     } 
   } 
   FreeSpace* last_info = info(this->last_chain);
@@ -179,13 +179,13 @@ FreeSpace* Simple::registerPointer(ptrdiff_t diff, size_t size) {
   }
   this->pointer_info_start -= m;
   last_info->size -= m;
-  this->setBlock(move(this->base, this->pointer_info_start), size, diff);
-  return info(move(this->base, this->pointer_info_start));
+  this->setBlock(add_diff(this->base, this->pointer_info_start), size, diff);
+  return info(add_diff(this->base, this->pointer_info_start));
 }
 
 void Simple::unregisterPointer(FreeSpace* memreg) {
   size_t m = sizeof(FreeSpace);
-  if (memreg == info(move(this->base, this->pointer_info_start))) {
+  if (memreg == info(add_diff(this->base, this->pointer_info_start))) {
     this->pointer_info_start += m;
     FreeSpace* last_info = info(this->last_chain);
     last_info->size += m;
@@ -198,7 +198,7 @@ void Simple::unregisterPointer(FreeSpace* memreg) {
 int Simple::unregisterPointer(ptrdiff_t diff, size_t size) {
   size_t m = sizeof(FreeSpace);
   for (size_t i = this->pointer_info_start; i < this->size - m; i+=m) {
-    FreeSpace* ptr_info = info(move(this->base, i));
+    FreeSpace* ptr_info = info(add_diff(this->base, i));
     if (ptr_info->diff == diff) {
       if (ptr_info->size == size) {
         throw std::runtime_error("");
@@ -212,7 +212,7 @@ int Simple::unregisterPointer(ptrdiff_t diff, size_t size) {
 
 void Simple::setFirstBlock(ptrdiff_t _diff) {
   size_t m = sizeof(FreeSpace);
-  FreeSpace* inf = info(move(this->base, this->size - m));
+  FreeSpace* inf = info(add_diff(this->base, this->size - m));
   inf->diff = _diff;
   inf->size = 1;
 }
@@ -222,9 +222,9 @@ void Simple::unite(void* cur) {
   void* next = NULL;
   FreeSpace* next_info = NULL;
   while(cur_info->diff > 0) {
-    next = move(this->base, cur_info->diff);
+    next = add_diff(this->base, cur_info->diff);
     next_info = info(next);
-    if(move(cur, cur_info->size) == next) {
+    if(add_diff(cur, cur_info->size) == next) {
       cur_info->diff = next_info->diff;
       cur_info->size += next_info->size;
     }
@@ -236,12 +236,12 @@ void Simple::unite(void* cur) {
 
 void* Simple::findFirstBlock(void) {
   size_t m = sizeof(FreeSpace);
-  FreeSpace* inf = info(move(this->base, this->size - m));
+  FreeSpace* inf = info(add_diff(this->base, this->size - m));
   if (inf->size == 0) {
     return NULL;
   }
   else {
-    return move(this->base, inf->diff);
+    return add_diff(this->base, inf->diff);
   }
 }
 
@@ -252,7 +252,7 @@ Simple::Simple(void *base, size_t size) {
   this->size = new_size;
   cleanup(this->base, this->size);
   this->setBlock(base, this->size - m, 0);
-  this->setBlock(move(base, this->size - m), 1, 0);
+  this->setBlock(add_diff(base, this->size - m), 1, 0);
   this->pointer_info_start = this->size - m;
   this->last_chain = base;
 }
@@ -267,7 +267,7 @@ Pointer Simple::alloc(size_t N) {
     return Pointer(this->base);
   }
 
-  FreeSpace* start_info = info(move(this->base, this->size - m));
+  FreeSpace* start_info = info(add_diff(this->base, this->size - m));
   if (start_info->size == 0) {
     throw AllocError(AllocErrorType::NoMemory, "i have eaten all your memory. it's a joke. No place...");
   }
@@ -307,7 +307,7 @@ void Simple::realloc(Pointer &pt, size_t N) {
   }
   FreeSpace p = *(pt.ptr);
   size_t m = sizeof(FreeSpace);
-  FreeSpace* start_info = info(move(this->base, this->size - m));
+  FreeSpace* start_info = info(add_diff(this->base, this->size - m));
   if (p.size == N) {
     return;
   }
@@ -315,7 +315,7 @@ void Simple::realloc(Pointer &pt, size_t N) {
     /*
      * add to chain free space
      */
-    void* ptr = move(move(this->base, p.diff), N);
+    void* ptr = add_diff(add_diff(this->base, p.diff), N);
     if (start_info->size == 0) {
       /*
        * If there are no free space
@@ -333,7 +333,7 @@ void Simple::realloc(Pointer &pt, size_t N) {
       prev = this->findPrevious(base, ptr);
     }
     else {
-      prev = move(this->base, this->size - m);
+      prev = add_diff(this->base, this->size - m);
     }
     FreeSpace* cur_info = info(ptr);
     FreeSpace* prev_info = info(prev);
@@ -351,25 +351,25 @@ void Simple::realloc(Pointer &pt, size_t N) {
     throw AllocError(AllocErrorType::NoMemory, "i have eaten all your memory!");
   }
 
-  void* ptr = move(this->base, p.diff);
+  void* ptr = add_diff(this->base, p.diff);
   void* base = this->findFirstBlock();
   void* prev = NULL;
   if (base < ptr) {
     prev = this->findPrevious(base, ptr);
   }
   else {
-    prev = move(this->base, this->size - m);
+    prev = add_diff(this->base, this->size - m);
   } // as we do not change a size of previous 
   void* next = NULL;
   FreeSpace* prev_info = info(prev);
   FreeSpace* next_info = NULL;
 
   if (prev_info->diff > 0) {
-    next = move(this->base, prev_info->diff);
+    next = add_diff(this->base, prev_info->diff);
     this->unite(next);
     next_info = info(next);
     if (
-        move(ptr, p.size) == next && N <= p.size + next_info->size && 
+        add_diff(ptr, p.size) == next && N <= p.size + next_info->size && 
         (next != this->last_chain || N + 2 * m <= p.size + next_info->size)
       ) {
       /*
@@ -379,7 +379,7 @@ void Simple::realloc(Pointer &pt, size_t N) {
         /*
          * Delete part of next
          */
-        void* new_ptr = move(ptr, N);
+        void* new_ptr = add_diff(ptr, N);
         FreeSpace* new_ptr_info = info(new_ptr);
         new_ptr_info->diff = next_info->diff;
         new_ptr_info->size = p.size + next_info->size - N;
@@ -396,7 +396,7 @@ void Simple::realloc(Pointer &pt, size_t N) {
          */
         prev_info->diff = next_info->diff;
         p.size = N;
-        if (prev_info->diff == 0 && prev != move(this->base, this->size - m)) {
+        if (prev_info->diff == 0 && prev != add_diff(this->base, this->size - m)) {
           this->last_chain = prev;
         }
         return;
@@ -407,8 +407,8 @@ void Simple::realloc(Pointer &pt, size_t N) {
    * There is no way to extend p not using changing p.diff
    */
   Pointer q = alloc(N);
-  char* dst = reinterpret_cast<char*>(move(this->base, q.ptr->diff));
-  char* src = reinterpret_cast<char*>(move(this->base, p.diff));
+  char* dst = reinterpret_cast<char*>(add_diff(this->base, q.ptr->diff));
+  char* src = reinterpret_cast<char*>(add_diff(this->base, p.diff));
   for (size_t i = 0; i < N; i++) {
     dst[i] = src[i];
   }
@@ -426,16 +426,16 @@ void Simple::free(Pointer &pt) {
   }
   FreeSpace p = *(pt.ptr);
   size_t m = sizeof(FreeSpace);
-  FreeSpace* start_info = info(move(this->base, this->size - m));
+  FreeSpace* start_info = info(add_diff(this->base, this->size - m));
   if (start_info->size == 0) {
     start_info->diff = p.diff;
     start_info->size = 1;
-    this->setBlock(move(this->base, p.diff), p.size, 0);
-    this->last_chain = move(this->base, p.diff);
+    this->setBlock(add_diff(this->base, p.diff), p.size, 0);
+    this->last_chain = add_diff(this->base, p.diff);
   }
   else {
     void* base = this->findFirstBlock();
-    void* ptr = move(this->base, p.diff);
+    void* ptr = add_diff(this->base, p.diff);
     FreeSpace* cur_info = info(ptr);
     FreeSpace* base_info = info(base);
     void* prev = this->findPrevious(base, ptr);
@@ -475,7 +475,7 @@ static void copy(void* src, void* dst, size_t size) {
 }
 
 void Simple::placeTo(FreeSpace* p_inf, ptrdiff_t where) {
-  copy(move(this->base, p_inf->diff), move(this->base, where), p_inf->size);
+  copy(add_diff(this->base, p_inf->diff), add_diff(this->base, where), p_inf->size);
   p_inf->diff = where;
 }
 
@@ -484,7 +484,7 @@ FreeSpace* Simple::getNext(ptrdiff_t l) {
   FreeSpace* res = NULL;
   ptrdiff_t mn = this->size;
   for (size_t i = this->pointer_info_start; i < this->size - m; i+=m) {
-    FreeSpace* inf = info(move(this->base, i));
+    FreeSpace* inf = info(add_diff(this->base, i));
     //std::cout << inf->diff << " " << inf->size << std::endl;
     if (inf->diff >= l && inf->diff < mn && inf->size > 0) {
       res = inf;
@@ -496,12 +496,12 @@ FreeSpace* Simple::getNext(ptrdiff_t l) {
 
 void Simple::defrag() {
   size_t m = sizeof(FreeSpace);
-  FreeSpace* start_info = info(move(this->base, this->size - m));
+  FreeSpace* start_info = info(add_diff(this->base, this->size - m));
   void* base = NULL;
   if (start_info->size == 0) {
     return;
   }
-  base = move(this->base, start_info->diff);
+  base = add_diff(this->base, start_info->diff);
   void* cur = base;
   FreeSpace* cur_info = info(cur);
   while (cur_info->diff > 0) {
@@ -510,7 +510,7 @@ void Simple::defrag() {
     if (cur_info->diff == 0) {
       break;
     }
-    cur = move(this->base, cur_info->diff);
+    cur = add_diff(this->base, cur_info->diff);
   }
   /*
    * moving
@@ -525,7 +525,7 @@ void Simple::defrag() {
     this->placeTo(inf, len);
     len += inf->size;
   }
-  this->last_chain = move(this->base, len);
+  this->last_chain = add_diff(this->base, len);
   this->setFirstBlock(dst(this->base, this->last_chain));
   this->setBlock(this->last_chain, this->pointer_info_start - len, 0);
 }
@@ -538,7 +538,7 @@ std::string Simple::dump() const {
   std::stringstream buf;
   FreeSpace* inf = NULL;
   for (size_t i = this->pointer_info_start; i < this->size - m; i+=m) {
-    inf = info(move(this->base, i));
+    inf = info(add_diff(this->base, i));
     buf << inf->diff << " " << inf->size << std::endl;
   }
   return buf.str();
