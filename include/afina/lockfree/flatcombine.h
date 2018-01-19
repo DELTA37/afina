e153ec571893d0b842b25544d89f4c879f7029ea
delta37 <delta37@delta37.delta37>
2018-01-19 17:15:59 +0300

    flatcombine

--- include/afina/lockfree/flatcombine.h
+++ include/afina/lockfree/flatcombine.h
@@ -22,42 +22,46 @@ protected:
   volatile std::atomic<Record*> head;
 public:
   void enqueque_slot(const Slot& c) {
-    Record* local_node = node.get();
-    if (local_node == NULL) {
-      local_node = new Record();
-      local_node->slot = c;
-      node.set(local_node);
+    publicate(c);
+    if (acquire_lock()) {
+      flat_combine();
+      release_lock();
     }
-    
   }
 
   void acquire_lock(void) {
     uint8_t _count = 0;
+    uint8_t _old = 0;
     while(count.compare_exchange_weak(_count, _count | 1, std::memory_order_release, std::memory_order_relaxed)) { // пытаемся установить мьютекс
       _count &= ~uint8_t(1); // ожидаем сброшенный мьютекст
+      Record* local_record = node.get();
+      if (!local_record || !local_record->active) {
+        return false;
+      }
       std::this_thread::yield();
     }
     _count += 2; // подсчет эры, несбрасывая мьютекса
+    return true;
   }
 
   void release_lock(void) {
-    count.store(count & 0);
+    count.store(count & 0, std::memory_order_relaxed);
   }
 
-  void apply(Slot c) {
+  void publicate(const Slot& c) {
     Record* local_node = node.get();
     if (local_node == NULL) {
       local_node = new Record();
       local_node->slot = c;
       node.set(local_node);
     }
-    while(!(local_node->next = head.compare_exchange_weak(local_node->next, local_node, std::memory_order_release, std::memory_order_relaxed))) {}
-    // added node
+    while(!(head.compare_exchange_weak(local_node->next, local_node, std::memory_order_release, std::memory_order_relaxed))) {}
   }
+
   FC(void);
   ~FC(void);
 
-  virtual void flat_combine(void);
+  virtual void flat_combine(void) {}
 };
 
 } // Afina
